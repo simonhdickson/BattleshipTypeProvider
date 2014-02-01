580dd21adb22d4ae5f5335429d2c2754b980fdd1
Simon Dickson <simonhdickson@gmail.com>
2014-02-01 11:06:28 +0000

    Initial commit

---
+++ BattleshipsTypeProvider/BattleshipProvider.fs
@@ -0,0 +1,51 @@
+﻿module Mavnn.Blog.TypeProvider
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System
+open System.Reflection
+open ItWorksOnMyMachine.FSharpShip
+
+[<TypeProvider>]
+type BattleshipProvider (config : TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces ()
+
+    let ns = "Game.Battleships"
+    let asm = Assembly.GetExecutingAssembly()
+    let serviceType = ProvidedTypeDefinition(asm, ns, "ProvidedTypes", Some typeof<obj>, HideObjectMethods = true)
+    let g = new Game(10, [2;2;3])
+    let possibleTargets =
+        seq {
+            for x in [0..9] do
+                for y in [0..9] do
+                    yield x, y
+        }
+
+    let createDocs target =
+        match g.Test target with
+        | Hit  -> "Hit!"    
+        | Miss -> "Miss!"
+        | Sunk -> "Sunk!"
+        | Won -> "Won!"
+
+    let rec createProperty parent list (x, y) =
+        let name = sprintf "%i,%i" x y
+        let propertyType = ProvidedTypeDefinition(name, Some typeof<obj>)
+        let newList = Seq.where (fun i -> i <> (x, y)) list
+        propertyType.AddMembersDelayed(fun () -> Seq.map (createProperty propertyType newList) newList |> List.ofSeq)
+        let property = ProvidedProperty(name, propertyType, GetterCode = (fun args -> <@@ new obj() @@>))
+        property.AddXmlDocDelayed(fun () -> createDocs (x, y))
+        parent.AddMember(propertyType)
+        property
+
+    let createTypes () =
+        let myType = ProvidedTypeDefinition(asm, ns, "Battleship", Some typeof<obj>)
+        myType.AddMember(ProvidedConstructor([], InvokeCode = fun args -> <@@ new obj() @@>))
+        myType.AddMembersDelayed(fun () -> Seq.map (createProperty serviceType possibleTargets) possibleTargets |> List.ofSeq)
+        [myType]
+
+    do
+        this.AddNamespace(ns, serviceType :: createTypes())
+
+[<assembly:TypeProviderAssembly>]
+do ()
---
+++ BattleshipsTypeProvider/Battleships.fs
@@ -0,0 +1,97 @@
+﻿module ItWorksOnMyMachine.FSharpShip
+open Microsoft.FSharp.Control
+
+type Agent<'Msg> = MailboxProcessor<'Msg>
+
+type Point =
+    | Point of (int * int)
+
+type Ship =
+    | Ship of Point list
+
+type ShootResult =
+    | Miss
+    | Hit
+    | Sunk
+    | Won
+
+type Message =
+    | Shoot of (int * int) * AsyncReplyChannel<ShootResult>
+    | Test of (int * int) * AsyncReplyChannel<ShootResult>
+
+type Game (shipList: Ship list) =
+    let delete target (Ship points) =
+        points |> List.filter (fun x -> x <> Point(target))
+               |> Ship
+
+    let deleteHit target =
+        let isNotSunk (Ship points) = points <> []
+        List.map (delete target) >> List.filter isNotSunk
+
+    let onCollision target ships =
+        let newShips = ships |> deleteHit target
+        match newShips.Length with
+        | 0 -> (Won, [])
+        | n when n = ships.Length -> (Hit, newShips)
+        | _ -> (Sunk, newShips)
+
+    let fleet = Agent.Start(fun inbox ->
+        let rec loop (ships: Ship list) =
+            async {
+                let! message = inbox.Receive()
+                match message with
+                | Shoot (target, replyChannel) ->
+                    if Point(target) |> Game.anyHit ships then
+                        let (output, newShips) = onCollision target ships
+                        replyChannel.Reply output
+                        return! loop newShips
+                    else
+                        replyChannel.Reply Miss
+                        return! loop ships
+                | Test (target, replyChannel) ->
+                    match Point(target) |> Game.anyHit ships with
+                    | true  -> replyChannel.Reply Hit
+                    | false -> replyChannel.Reply Miss
+                    return! loop ships
+            }
+        loop shipList )
+
+    static member private anyHit ships point =
+        let isShipHit (Ship points) = points |> Seq.exists (fun x -> x = point)
+        ships |> List.exists isShipHit
+
+    static member private generateShip (size, length) =
+        let rnd = System.Random()
+        let horizontal = rnd.Next 2 = 1
+        let constant = rnd.Next size
+        let vary = rnd.Next (size - length)
+        let dots = [vary..vary + length - 1]
+        let mapYFun = fun y -> Point(constant, y)
+        let mapXFun = fun x -> Point(x, constant)
+        if horizontal then
+            dots |> List.map mapYFun |> Ship
+        else
+            dots |> List.map mapXFun |> Ship
+
+    static member private generateLegalShip (size, length, existingShips) =
+        let (Ship points) = Game.generateShip (size, length)
+        if points |> List.exists (Game.anyHit existingShips) then
+            Game.generateLegalShip (size, length, existingShips)
+        else
+            Ship(points)
+
+    static member private generateShips size lengthList =
+        let mapFun = fun (ships: Ship list) x -> (Game.generateLegalShip (size, x, ships)) :: ships
+        lengthList |> List.fold mapFun []
+
+    new () = Game ( [Ship([Point(1,2); Point(2,2)]); Ship([Point(4,5); Point(4,6)])] )
+
+    new (size: int, lengthList: int list) =
+        let ships = Game.generateShips size lengthList
+        Game ships
+
+    member this.Shoot target =
+        fleet.PostAndReply (fun replyChannel -> Shoot (target, replyChannel))
+
+    member this.Test target =
+        fleet.PostAndReply (fun replyChannel -> Test (target, replyChannel))
